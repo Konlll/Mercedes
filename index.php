d0c5ab4298a1f3c93a6c55f852debeb11c12fe5c
Konlll <hungarokornel@gmail.com>
2019-04-07 19:45:30 +0200

    index.php létrehozása

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Mercedes</title>
+    </head>
+    <body>
+        <div class="menu-wrap">
+            <input type="checkbox" class="toggler">
+            <div class="hamburger">
+                <div></div>
+            </div>
+            <div class="menu">
+                <div>
+                    <div>
+                        <ul>
+                            <li><a href="">Mercedes</a></li>
+                            <li><a href="">Mercedes</a></li>
+                            <li><a href="">Mercedes</a></li>
+                            <li><a href="">Mercedes</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
